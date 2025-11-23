e3a18214ea448f70c7db368d4090525157e302c1
FormulaCashew <acerty19@gmail.com>
2025-11-23 16:46:08 -0600

    Added knn class from past exam

--- Class_implementations.py
+++ Class_implementations.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import random
+from collections import Counter
 from sklearn.cluster import KMeans
 
 class Graphics:
@@ -325,3 +326,70 @@ class DataProcessor:
         test_df = self.df.iloc[test_indexes].reset_index(drop=True).copy()
         
         return train_df, test_df 
+
+class KNN:
+    """
+    Class KNN_Model with methods to implement KNN_Model
+
+    """
+
+    def __init__(self, k: int = 3):
+        """
+        Constructor of KNN_Model
+        Args:
+            k (int): number of neighbors to use, defaults to 3, it is recommended to use an odd number
+        """
+        self._outputs_train = None
+        self._inputs_train = None
+        self._k = k
+
+    def set_k(self, k: int):
+        """ Simple setter for k neighbors"""
+        self._k = k
+
+    def store(self, inputs_train, outputs_train):
+        """
+        Stores training data
+        Args:
+            inputs_train (pandas.DataFrame): training data inputs
+            outputs_train (pandas.DataFrame): training data outputs
+        """
+        self._inputs_train = np.array(inputs_train)
+        self._outputs_train = np.array(outputs_train)
+        print("Training data stored")
+
+    def predict(self, inputs):
+        """
+        Predicts an output given various inputs data
+        Args:
+            inputs (pandas.DataFrame): input data
+        Returns:
+            pandas.DataFrame: predicted outputs
+        """
+        inputs_arr = np.array(inputs)
+        predictions = [self.predict_single(input_row) for input_row in inputs_arr] # List with various outputs
+        return predictions
+
+    def predict_single(self, input_test):
+        """
+        Function to predict the output given single input data row
+        Args:
+            input_test (pandas.DataFrame): input data, needs to have the Attributes of the training data
+        Returns:
+            pandas.DataFrame: predicted outputs
+        """
+        distances = []
+        for i, input_row in enumerate(self._inputs_train):
+            dist = self.euclidean_distance(input_row, input_test)   # measure distance between given inputs and local data
+            distances.append((dist, self._outputs_train[i])) # save distance and output data
+
+        sorted_distances = sorted(distances, key=lambda x: x[0])
+        nearest_neighbors = sorted_distances[:self._k] # get only the distances up to k
+        nearest_neighbor_labels = [neighbor[1] for neighbor in nearest_neighbors]
+        survived = Counter(nearest_neighbor_labels).most_common(1)[0] # Check for most common output in the neighbors
+        return survived[0]
+
+
+    @staticmethod
+    def euclidean_distance(p1, p2):
+        return np.sqrt(np.sum((p1 - p2)**2))
